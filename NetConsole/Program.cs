20b0a80550d08e19d9e6616cce5818fa99940a62
Аслан Нурсеитов <grooves97@gmail.com>
2019-03-06 12:28:34 +0600

    Json serialized

---
+++ NetConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+using System.Runtime.Serialization;
+
+namespace NetConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RootObject temp = new RootObject();
+
+            var client = new WebClient();            
+            var json = client.DownloadString("http://hp-api.herokuapp.com/api/characters");
+            var rootDeserialized = JsonConvert.DeserializeObject<RootObject[]>(json);
+
+        }
+    }
+}
+
